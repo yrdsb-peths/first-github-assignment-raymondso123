e27b12d9aef47a926b49a313cdd9f1bc8ae53195
raymondso123 <167659047+raymondso123@users.noreply.github.com>
2024-04-21 00:03:57 -0400

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,1 +1,15 @@
+public class Dog {
+    private String name;
+    private String breed;
+    private int age;
 
+    public Dog(String Name, String Breed, int Age) {
+        name = Name;
+        breed = Breed;
+        age = Age;
+    }
+
+    public String toString() {
+        return "The " + age + " year old " + breed + " is called " + name + "!"
+    }
+}
