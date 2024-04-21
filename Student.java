6a68b311d6f804036a87f21f663e5ddaf955b0bf
raymondso123 <167659047+raymondso123@users.noreply.github.com>
2024-04-20 23:44:04 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,16 @@
-
+public class Student
+{
+    private String name;
+    private int cohort;
+    
+    public Student(String Name, int grade)
+    {
+        name = Name;
+        cohort = grade;
+    }
+    
+    public String toString()
+    {
+        return name + " is a " + cohort + " student!";
+    }
+}
