8f35cf901598d78311a20eca5c21f2a50ccf0ff7
raymondso123 <167659047+raymondso123@users.noreply.github.com>
2024-04-20 23:56:16 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -3,18 +3,20 @@ public class Student
     private String name;
     private int cohort;
     private boolean honors;
+    private int hours;
     
-    public Student(String Name, int grade, boolean hons)
+    public Student(String Name, int grade, boolean hons, int hrs)
     {
         name = Name;
         cohort = grade;
         honors = hons;
+        hours = hrs;
     }
 
     private String honString = honors ? "" : " honors";
     
     public String toString()
     {
-        return name + " is a grade " + cohort + honString + " student!";
+        return name + " is a grade " + cohort + honString + " student with " + hours + " of volunteering experience!";
     }
 }
